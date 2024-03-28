ed7e14e86498dc0eed7904447c161b42c0e0c312
CryoCode <owenramaekers2006@gmail.com>
2024-03-28 09:22:14 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
 </head>
 <body>
     <form>
-        <label for="steen">Steen:</label>
-        <input type="checkbox" name="steen" id="steen" onchange="document.getElementById('papier').checked = false; document.getElementById('schaar').checked = false;"><br>
-        <label for="papier">Papier:</label>
-        <input type="checkbox" name="papier" id="papier" onchange="document.getElementById('steen').checked = false; document.getElementById('schaar').checked = false;"><br>
-        <label for="schaar">Schaar:</label>
-        <input type="checkbox" name="schaar" id="schaar" onchange="document.getElementById('steen').checked = false; document.getElementById('papier').checked = false;"><br>
+        <button type="submit" name="spel" value="steen">Steen</button>
+        <button type="submit" name="spel" value="papier">Papier</button>
+        <button type="submit" name="spel" value="schaar">Schaar</button>
     </form>
+        
 </body>
 </html>
